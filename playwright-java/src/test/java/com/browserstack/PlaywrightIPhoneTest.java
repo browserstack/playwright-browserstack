3d43d57d715f6426e37b57cf29bfcfa54b0412c1
kamal-kaur04 <38219887+kamal-kaur04@users.noreply.github.com>
2022-03-15 15:02:38 +0530

    minor fix

--- playwright-java/src/test/java/com/browserstack/PlaywrightIPhoneTest.java
+++ playwright-java/src/test/java/com/browserstack/PlaywrightIPhoneTest.java
@@ -8,16 +8,16 @@ import java.net.URLEncoder;
 public class PlaywrightIPhoneTest {
     public static void main(String[] args) {
         try (Playwright playwright = Playwright.create()) {
-            JsonObject jsonObject = new JsonObject();
-            jsonObject.addProperty("browser", "playwright-webkit");    // allowed browsers are `chrome`, `edge`, `playwright-chromium`, `playwright-firefox` and `playwright-webkit`
-            jsonObject.addProperty("browser_version", "latest");
-            jsonObject.addProperty("name", "Test on Playwright emulated Devices");
-            jsonObject.addProperty("build", "playwright-java-4");
-            jsonObject.addProperty("browserstack.username", "BROWSERSTACK_USERNAME");
-            jsonObject.addProperty("browserstack.accessKey", "BROWSERSTACK_ACCESS_KEY");
+            JsonObject capabilitiesObject = new JsonObject();
+            capabilitiesObject.addProperty("browser", "playwright-webkit");    // allowed browsers are `chrome`, `edge`, `playwright-chromium`, `playwright-firefox` and `playwright-webkit`
+            capabilitiesObject.addProperty("browser_version", "latest");
+            capabilitiesObject.addProperty("name", "Test on Playwright emulated Devices");
+            capabilitiesObject.addProperty("build", "playwright-java-4");
+            capabilitiesObject.addProperty("browserstack.username", "BROWSERSTACK_USERNAME");
+            capabilitiesObject.addProperty("browserstack.accessKey", "BROWSERSTACK_ACCESS_KEY");
 
             BrowserType chromium = playwright.chromium();
-            String caps = URLEncoder.encode(jsonObject.toString(), "utf-8");
+            String caps = URLEncoder.encode(capabilitiesObject.toString(), "utf-8");
             String ws_endpoint = "wss://cdp.browserstack.com/playwright?caps=" + caps;
 
             Browser browser = chromium.connect(ws_endpoint);
@@ -29,25 +29,31 @@ public class PlaywrightIPhoneTest {
                     .setHasTouch(true));
 
             Page page = context.newPage();
-            page.navigate("https://www.google.co.in/");
-            Locator locator = page.locator("[aria-label='Search']");
-            locator.click();
-            page.fill("[aria-label='Search']", "BrowserStack");
-            page.keyboard().press("Enter");
-            page.locator("[aria-current='page']").waitFor();
-            String title = page.title();
+            try {
+                page.navigate("https://www.google.co.in/");
+                Locator locator = page.locator("[aria-label='Search']");
+                locator.click();
+                page.fill("[aria-label='Search']", "BrowserStack");
+                page.keyboard().press("Enter");
+                page.locator("[aria-current='page']").waitFor();
+                String title = page.title();
 
-            Object result;
-            if (title.equals("BrowserStack - Google Search")) {
-                // following line of code is responsible for marking the status of the test on BrowserStack as 'passed'. You can use this code in your after hook after each test
-                result = page.evaluate("_ => {}", "browserstack_executor: { \"action\": \"setSessionStatus\", \"arguments\": { \"status\": \"passed\", \"reason\": \"Title matched\"}}");
-            } else {
-                result = page.evaluate("_ => {}", "browserstack_executor: { \"action\": \"setSessionStatus\", \"arguments\": { \"status\": \"failed\", \"reason\": \"Title did not matched\"}}");
+                if (title.equals("BrowserStack - Google Search")) {
+                    // following line of code is responsible for marking the status of the test on BrowserStack as 'passed'. You can use this code in your after hook after each test
+                    markTestStatus("passed", "Title matched", page);
+                } else {
+                    markTestStatus("failed", "Title did not match", page);
+                } 
+            } catch (Exception err) {
+                    markTestStatus("failed", err.getMessage(), page);
             }
-
             browser.close();
         } catch (UnsupportedEncodingException e) {
             System.out.println(e);
         }
+    }
+    public static void markTestStatus(String status, String reason, Page page) {
+        Object result;
+        result = page.evaluate("_ => {}", "browserstack_executor: { \"action\": \"setSessionStatus\", \"arguments\": { \"status\": \"" + status + "\", \"reason\": \"" + reason + "\"}}");
     }
 }
